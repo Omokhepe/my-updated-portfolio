76ce6eb7e5f00270938c29cc66f68bb814cc3498
omoh_khepe <fimobu@gmail.com>
2025-09-03 14:58:59 +0800

    project slider section image modification

--- src/component/ImageSlider.jsx
+++ src/component/ImageSlider.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import Carousel from "react-img-carousel";
-import "react-img-carousel/lib/carousel.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "./ImageSlider.css";
 import {projects} from "../constant/data.js";
-
 
 export default function ImageSlider() {
     const [activeIndex, setActiveIndex] = useState(null);
@@ -13,41 +15,49 @@ export default function ImageSlider() {
     };
 
     return (
-        <Carousel
-            autoplay={true}
-            autoplaySpeed={3000}
-            infinite={true}
-            dots={true}
-            arrows={true}
-            slideWidth="700px"
-            swipe={true}
-            draggable={true}
+        <Swiper
+            modules={[Autoplay, Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true, // 👈 this pauses autoplay on hover
+            }}
+            loop={true}
+            breakpoints={{
+                640: { slidesPerView: 1 },
+                768: { slidesPerView: 2 },
+                1024: { slidesPerView: 2 },
+            }}
         >
             {projects.map((item, i) => (
-                <div
-                    className={`slide ${activeIndex === i ? "show-overlay" : ""}`}
-                    key={i}
-                    onClick={() => handleTap(i)}
-                >
-                    <img src={item.image} alt={`slide-${i}`} />
-                    <div className="overlay">
-                        {item.description}
-                        {
-                            item.site !== '' && (
-                                <a href={item.site} target="_blank" className="view-project">
-                                    <div className="overlay-content">
-                                        <span className="material-symbols-outlined icon">link</span>
-                                        <span className="textPresetBold text">View Project</span>
-                                    </div>
-                                </a>
-                            )
-                        }
-
+                <SwiperSlide key={i}>
+                    <div
+                        className={`slide ${activeIndex === i ? "show-overlay" : ""}`}
+                        onClick={() => handleTap(i)}
+                        onMouseLeave={() => setActiveIndex(null)}
+                    >
+                        <img src={item.image} alt={`slide-${i}`} />
+                        <div className="overlay">
+                            {item.description}
+                            {
+                                item.site !== '' && (
+                                    <a href={item.site} target="_blank" className="view-project">
+                                        <div className="overlay-content">
+                                            <span className="material-symbols-outlined icon">link</span>
+                                            <span className="textPresetBold text">View Project</span>
+                                        </div>
+                                    </a>
+                                )
+                            }
+                        </div>
                     </div>
-                </div>
+                </SwiperSlide>
             ))}
-        </Carousel>
+        </Swiper>
     );
 }
-
 
